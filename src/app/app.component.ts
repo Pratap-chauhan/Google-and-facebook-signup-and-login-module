9e2da82dde6e147fdca182f9e23225bff7c0d240
pratap_chauhan <pratap.chauhan@gmail.com>
2018-03-24 16:19:58 +0530

    comment crousel added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,24 @@ import { Component ,OnInit } from '@angular/core';
 export class AppComponent implements  OnInit {
   title = 'app';
   dummi;
+  showCommentIndex=0;
  jsonvalues=[]
+showComment;
+CommentDetails=[
+  {
+    comment:"It was so worth it to get a Nanodegree. Seeing that first paycheck, I couldn’t believe it. I never thought,I’d be here. It’s like a dream.",
+    author:"JENNIFER, Graduate"
+  },
+  {
+    comment:"Before Udacity, I was a high school math teacher with no CS background. Now I'm an Android Developer at Capital One, doing something I love while earning twice as much.",
+    author:"BEN, GRADUATE"
+  },
+  {
+    comment:"Udacity has empowered me to be a better software engineer. They have given me opportunities that I would not have if I had to rely on the traditional education system.",
+    author:" JONATHAN, GRADUATE"
+
+  }
+]
   constructor(){
   this.dummi = [{
     "Software Products": [
@@ -71,39 +88,32 @@ console.log(">>>>>>>>.dummi",this.dummi)
   }
   ngOnInit()
   {
+   this.showComment=this.CommentDetails[this.showCommentIndex]
     console.log(">>>",this.dummi.length,this.dummi[3])
     for(let i=0;i<this.dummi.length;i++)
 this.getData(this.dummi[i],i)
   }
 
   getData(jsonObject,i){
-
-// var jsonObjectKeys =Object.keys(jsonObject)
-// this.jsonvalues.push({values:jsonObject,key:'i'})
-// if(jsonObject[i].length){
-//   this.getData(jsonObject[i],i)
-// }
-// console.log("jsonvalues",this.jsonvalues)
-// console.log(">>>>jsonObject",jsonObject)
-// if(jsonObject.length>0)
-// {
-//   for(let j=0;j<jsonObject.length;i++)
-//   this.getData(jsonObject[0],i++)
-  // var jsonObjectKeys =Object.keys(jsonObject[i][0])
-  // console.log(">>>>>>>>>",jsonObjectKeys)
-// }
-// if(jsonObjectKeys)
-// {
-  
-  // this.jsonvalues.push({value:jsonObjectKeys,keys:i});
-  // i++;
-//   this.getData(jsonObjectKeys,i)
-// }
-// else{
-  // this.jsonvalues.push({value:jsonObject,keys:i});
-//   return
-// }
-
   }
   // this.getData(dummi)
+  
+  next(){
+    let CommentDetailslength=this.CommentDetails.length;
+   
+    if(this.showCommentIndex < CommentDetailslength-1){
+      this.showCommentIndex++;
+      this.showComment = this.CommentDetails[this.showCommentIndex]
+    }
+  }
+
+previous(){
+
+  let CommentDetailslength=this.CommentDetails.length;
+  if(this.showCommentIndex > 0){
+    this.showCommentIndex--;
+    this.showComment = this.CommentDetails[this.showCommentIndex]
+  }
+}
+
 }
